39892d9c5923563a3eea4e88b61538b0a0a87397
Cagatay Eralp <cagatayeralp86@gmail.com>
2022-12-28 14:04:42 +0300

    initial fixes

---
+++ Readme.md
@@ -0,0 +1,18 @@
+MQX Data Layer Project
+
+Project uses 
+
+    -Python 3.8
+    -Flask==2.2.2
+    -flask-restx==1.0.3
+
+Running Project
+
+    -Option 1 Run DockerFile
+    -Option 2 run command : python -m flask run
+
+ENV Variables
+
+    -AWS-DEFAULT-REGION
+    -AWS-ACCESS-KEY
+    -AWS-SECRET-ACCESS-KEY
--- app.py
+++ app.py
@@ -1,21 +1,61 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restx import Resource, Api
+import awswrangler as wr
+import boto3
+import pandas as pd
+import os
 
 api = Api()
 
 app = Flask(__name__)
 api.init_app(app)
 
+boto3.setup_default_session(aws_access_key_id=os.environ['AWS-ACCESS-KEY'], aws_secret_access_key=os.environ['AWS-SECRET-ACCESS-KEY'])
+s3_client = boto3.client('s3', aws_access_key_id=os.environ['AWS-ACCESS-KEY'], aws_secret_access_key=os.environ['AWS-SECRET-ACCESS-KEY'])
+athena_session = boto3.Session(region_name=os.environ['AWS-DEFAULT-REGION'], aws_access_key_id=os.environ['AWS-ACCESS-KEY'], aws_secret_access_key=os.environ['AWS-SECRET-ACCESS-KEY'])
 
-@api.route('/test<id>')
-@api.doc(params={'id': 'An ID'})
-class MyResource(Resource):
-    def get (self, id):
-        return 'hello test' + id
+@api.route('/s3Example')
+class S3Example(Resource):
+    @api.doc(params={'key': 'key', 'bucket': 'bucket'})
+    def get(self):
+        bucket = request.args.get('bucket')
+        key = request.args.get('key')
+        path = f"s3://{bucket}/{key}"
+        try:
+            df = wr.s3.read_csv(path=path)
+        except Exception as e:
+            return str(e)
+        lst = df.head(5)
+        return lst.to_json()
 
-    @api.doc(responses={403: 'Not Authorized'})
-    def post(self, id):
-        api.abort(403)
+    @api.doc(params={'key': 'key', 'bucket': 'bucket'})
+    def post(self):
+        lst = ['Java', 'Python', 'C', 'C++', 'JavaScript', 'Swift', 'Go']
+        df = pd.DataFrame(lst)
+        bucket = request.args.get('bucket')
+        key = request.args.get('key')
+        path=f"s3://{bucket}/{key}"
+        try:
+            wr.s3.to_csv(df, path)
+            #s3_client.put_object(Body=df.to_csv(), Bucket=filepath, Key='my/test.csv')
+        except Exception as e:
+            return str(e)
+        return "successfully upload sample file to requested path"
+
+
+@api.route('/athenaExample<dbname>/<bucket>')
+@api.doc(params={'dbname': 'dbname', 'bucket': 'bucket'})
+class GlueCatalogExample(Resource):
+    def get(self, dbname, bucket):
+        try:
+            query = "SELECT * from processed_tv_guide WHERE year = '2021' and month = '11' and day = '11' and channel_key ='teve2'"
+            df = wr.athena.read_sql_query(query, database=dbname, ctas_approach=False, unload_approach=True,
+                                          s3_output=f"s3://{bucket}/data/", boto3_session=athena_session)
+        except Exception as e:
+            return str(e)
+
+        return "success"
+
 
 if __name__ == '__main__':
     app.run()
